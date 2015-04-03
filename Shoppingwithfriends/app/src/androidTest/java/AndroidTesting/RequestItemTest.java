d040a0eaca78f89b2ee6457d8b411d5fac244e04
BhavaniJaladanki <bjaladanki3@gatech.edu>
2015-04-03 06:13:41 -0400

    Bhavani's code

---
+++ Shoppingwithfriends/app/src/androidTest/java/AndroidTesting/RequestItemTest.java
@@ -0,0 +1,87 @@
+package AndroidTesting;
+
+/**
+ * Created by Bhavani on 4/3/15.
+ */
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.ViewAsserts;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+import com.example.android.ShoppingWithFriends.RequestItem;
+import com.example.android.ShoppingWithFriends.R;
+
+public class RequestItemTest extends ActivityInstrumentationTestCase2<RequestItem> {
+
+    private RequestItem myActivity;
+    private EditText itemName;
+    private EditText itemPrice;
+    Button requestItemButton;
+
+    public RequestItemTest() {
+        super(RequestItem.class);
+    }
+
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        setActivityInitialTouchMode(true);
+
+        myActivity = getActivity();
+
+
+        itemName = (EditText) myActivity.findViewById(R.id.ItemName);
+        itemPrice = (EditText) myActivity.findViewById(R.id.MaxPrice);
+        requestItemButton = (Button) myActivity.findViewById(R.id.RequestItemButton);
+    }
+
+    /**
+     * Tests if all component of activity gets initialized.
+     */
+    public void testPreconditions() {
+        try {
+            setUp();
+        } catch (Exception e) {
+
+        };
+        assertNotNull(myActivity);
+        assertNotNull(itemName);
+        assertNotNull(itemPrice);
+        assertNotNull(requestItemButton);
+
+    }
+
+    /**
+     * Tests presence of Buttons
+     */
+    public void testButtonsLayout() {
+        try {
+            setUp();
+        } catch (Exception e) {
+
+        };
+        final View decorView = myActivity.getWindow().getDecorView();
+
+        ViewAsserts.assertOnScreen(decorView, requestItemButton);
+        ViewAsserts.assertOnScreen(decorView, itemName);
+        ViewAsserts.assertOnScreen(decorView, itemPrice);
+
+
+
+        final ViewGroup.LayoutParams layoutParams1 = requestItemButton.getLayoutParams();
+        assertNotNull(layoutParams1);
+        assertEquals(layoutParams1.width, WindowManager.LayoutParams.MATCH_PARENT);
+        assertEquals(layoutParams1.height, WindowManager.LayoutParams.WRAP_CONTENT);
+    }
+
+
+
+
+}
