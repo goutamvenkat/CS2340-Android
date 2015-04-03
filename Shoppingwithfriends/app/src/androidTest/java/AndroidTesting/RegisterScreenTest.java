de042d19ee786e93af6687cb58d3e0bb18e30f6f
karanprime <karanprime@gatech.edu>
2015-04-03 05:26:52 -0400

    Added My thing

---
+++ Shoppingwithfriends/app/src/androidTest/java/AndroidTesting/RegisterScreenTest.java
@@ -0,0 +1,89 @@
+package AndroidTesting;
+
+/**
+ * Created by KaranPrime on 4/3/15.
+ */
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.ViewAsserts;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+import com.example.android.ShoppingWithFriends.RegisterScreen;
+import com.example.android.ShoppingWithFriends.R;
+
+public class RegisterScreenTest extends ActivityInstrumentationTestCase2<RegisterScreen> {
+
+    private RegisterScreen myActivity;
+    private EditText mUserName;
+    private EditText mEmail;
+    private EditText mPassword;
+    private Button RegisterButton;
+
+    public RegisterScreenTest() {
+        super(RegisterScreen.class);
+    }
+
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        setActivityInitialTouchMode(true);
+
+        myActivity = getActivity();
+
+        mUserName = (EditText) myActivity.findViewById(R.id.RegisterUserNameEditText);
+        mEmail = (EditText) myActivity.findViewById(R.id.RegisterEmailEditText);
+        mPassword = (EditText) myActivity.findViewById(R.id.RegisterPasswordEditText);
+        RegisterButton = (Button) myActivity.findViewById(R.id.RegisterButton);
+    }
+
+    /**
+     * Tests if all component of activity gets initialized.
+     */
+    public void testPreconditions() {
+        try {
+            setUp();
+        } catch (Exception e) {
+
+        };
+        assertNotNull(myActivity);
+        assertNotNull(mUserName);
+        assertNotNull(mEmail);
+        assertNotNull(mPassword);
+        assertNotNull(RegisterButton);
+
+    }
+
+    /**
+     * Tests presence of Buttons
+     */
+    public void testButtonsLayout() {
+        try {
+            setUp();
+        } catch (Exception e) {
+
+        };
+        final View decorView = myActivity.getWindow().getDecorView();
+
+        ViewAsserts.assertOnScreen(decorView, RegisterButton);
+        ViewAsserts.assertOnScreen(decorView, mUserName);
+        ViewAsserts.assertOnScreen(decorView, mEmail);
+        ViewAsserts.assertOnScreen(decorView, mPassword);
+
+
+        final ViewGroup.LayoutParams layoutParams1 = RegisterButton.getLayoutParams();
+        assertNotNull(layoutParams1);
+        assertEquals(layoutParams1.width, WindowManager.LayoutParams.MATCH_PARENT);
+        assertEquals(layoutParams1.height, WindowManager.LayoutParams.WRAP_CONTENT);
+    }
+
+
+
+
+}
